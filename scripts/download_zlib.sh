4afda98d5f2f41253956f2cf024023db7a134307
Jiradet Ounjai <jiradet.jd@gmail.com>
2022-10-24 09:14:28 +0700

    fix broken url

--- scripts/download_zlib.sh
+++ scripts/download_zlib.sh
@@ -3,7 +3,7 @@
 # If any commands fail, fail the script immediately.
 set -ex
 
-wget https://www.zlib.net/zlib-1.2.12.tar.gz -O /tmp/zlib-1.2.12.tar.gz
+wget https://zlib.net/fossils/zlib-1.2.12.tar.gz -O /tmp/zlib-1.2.12.tar.gz
 tar -xvf /tmp/zlib-1.2.12.tar.gz --directory /tmp
 
 # Copy the directory into the correct place
